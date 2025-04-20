517c6b322e2647d542ca868bf88a6bed93b545e3
Hawk <usual663@gmail.com>
2025-04-20 13:00:05 +0530

    Added clean server.js for Render

--- server.js
+++ server.js
@@ -1,78 +1,40 @@
-// server.js
-
-const express = require('express');
-const http = require('http');
-const cors = require('cors');
-const { Server } = require('socket.io');
-require('dotenv').config();
+const express = require("express");
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
 
 const app = express();
-app.use(cors());
-
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: '*',
-    methods: ['GET', 'POST'],
-  },
-});
-
-const PORT = process.env.PORT || 5000;
-
-let waitingUsers = []; // Queue of sockets waiting to be matched
-let rooms = {}; // roomId: [socket1, socket2]
-
-io.on('connection', (socket) => {
-  console.log(`User connected: ${socket.id}`);
-
-  // Add user to queue
-  waitingUsers.push(socket);
-
-  // Try to match
-  if (waitingUsers.length >= 2) {
-    const user1 = waitingUsers.shift();
-    const user2 = waitingUsers.shift();
-
-    const roomId = `room-${user1.id}-${user2.id}`;
-    rooms[roomId] = [user1, user2];
-
-    user1.join(roomId);
-    user2.join(roomId);
-
-    user1.emit('match-found', { roomId, peerId: user2.id });
-    user2.emit('match-found', { roomId, peerId: user1.id });
-
-    console.log(`Matched: ${user1.id} & ${user2.id} in ${roomId}`);
+    origin: "*",
+    methods: ["GET", "POST"]
   }
+});
 
-  // Relay WebRTC signaling messages
-  socket.on('signal', ({ to, data }) => {
-    io.to(to).emit('signal', { from: socket.id, data });
+app.use(cors());
+
+// Basic test route
+app.get("/", (req, res) => {
+  res.send("Welcome to the Chitchat backend!");
+});
+
+// Socket.io logic
+io.on("connection", (socket) => {
+  console.log("User connected: " + socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected: " + socket.id);
   });
 
-  // Handle disconnects
-  socket.on('disconnect', () => {
-    console.log(`User disconnected: ${socket.id}`);
-    waitingUsers = waitingUsers.filter((s) => s.id !== socket.id);
-
-    for (const [roomId, users] of Object.entries(rooms)) {
-      if (users.find((s) => s.id === socket.id)) {
-        const other = users.find((s) => s.id !== socket.id);
-        if (other) {
-          other.leave(roomId);
-          other.emit('partner-disconnected');
-        }
-        delete rooms[roomId];
-        break;
-      }
-    }
+  socket.on("message", (data) => {
+    console.log("Message received:", data);
+    socket.broadcast.emit("message", data);
   });
 });
 
-app.get('/', (req, res) => {
-  res.send('Chitchat backend matchmaking running!');
-});
-
+// Start the server
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Chitchat backend running on port ${PORT}`);
 });
